926d99d26985770493d10642af1dce84ae676bfb
JSZeroDev <sam12057@gmail.com>
2026-09-17 00:13:58 +0900

    Linked List 7번 문제 풀이 완료 closes #15

--- Data-Structures/Linked_List/Q7_A_LL.c
+++ Data-Structures/Linked_List/Q7_A_LL.c
@@ -87,7 +87,20 @@ int main()
 
 void RecursiveReverse(ListNode **ptrHead)
 {
-	/* add your code here */
+    ListNode *first = *ptrHead, *rest = NULL;
+
+    if (first == NULL || first->next == NULL) {
+        return;
+    }
+    else {
+        rest = first->next;
+
+        RecursiveReverse(&rest);
+
+        first->next->next = first;
+        first->next = NULL;
+    }
+    *ptrHead = rest;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
